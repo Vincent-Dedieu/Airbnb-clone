ba92d470c3be7e757c4412453f6baacbc3d1dfa4
Vincent <d.vincent1@hotmail.fr>
2023-10-22 20:02:09 +0200

    listingcard 5h23 yt

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,7 +4,11 @@ import useCountries from "@/app/hooks/useCountry";
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
+import { format } from "date-fns";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -39,7 +43,46 @@ const ListingCard: React.FC<ListingCardProps> = ({
     [onAction, actionId, disabled]
   );
 
-  return <div>ListingCard</div>;
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div onClick={() => router.push(`/listings/${data.id}`)} className="col-span-1 cursor-pointer group">
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image fill alt="Listing" src={data.imageSrc} className="object-cover h-full w-full group-hover:scale-110" />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">{reservationDate || data.category} </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold"> € {price}</div>
+          {!reservation && <div className="font-light">/ night</div>}
+        </div>
+        {onAction && actionLabel && <Button disabled={disabled} small label={actionLabel} onClick={handleCancel} />}
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
