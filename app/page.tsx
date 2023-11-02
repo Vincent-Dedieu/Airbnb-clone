e4b14bdfc968d37f25e2549c4459bd5016dd05d0
Vincent <d.vincent1@hotmail.fr>
2023-11-02 13:57:34 +0100

    fix page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,9 @@ const Home = async ({ searchParams }: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return <ListingCard currentUser={currentUser} key={listing.id} data={listing} />;
-          })}
+          {listings.map((listing: any) => (
+            <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
+          ))}
         </div>
       </Container>
     </ClientOnly>
